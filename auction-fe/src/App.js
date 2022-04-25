c5e810b5f1cc9b7440740a7b946a853ee2eeb97e
Erica <erica.gedda@outlook.com>
2022-04-25 17:34:16 +0200

    created view components

--- auction-fe/src/App.js
+++ auction-fe/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+import Home from "./components/home.component";
+import Login from "./components/login.component";
+import Register from "./components/register.component";
+import AuthService from "./services/auth.service";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.logOut = this.logOut.bind(this);
+    this.state = {
+      showModeratorBoard: false,
+      showAdminBoard: false,
+      currentUser: undefined,
+    };
+  }
+  componentDidMount() {
+    const user = AuthService.getCurrentUser();
+    if (user) {
+      this.setState({
+        currentUser: user,
+      });
+    }
+  }
+  logOut() {
+    AuthService.logout();
+  }
+  render() {
+    return (
+      <Switch>
+        <Route exact path={["/", "/register"]} component={Register} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/home" component={Home} />
+      </Switch>
+    );
+  }
 }
-
 export default App;
