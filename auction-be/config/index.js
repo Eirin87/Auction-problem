cb066821fec070792f15ce75b4ac679c22cbfe4d
Erica <erica.gedda@outlook.com>
2022-04-25 17:29:07 +0200

    added db config

---
+++ auction-be/config/index.js
@@ -0,0 +1,20 @@
+const dbConfig = require("./db.config.js");
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+  host: dbConfig.HOST,
+  dialect: dbConfig.dialect,
+  operatorsAliases: false,
+  pool: {
+    max: dbConfig.pool.max,
+    min: dbConfig.pool.min,
+    acquire: dbConfig.pool.acquire,
+    idle: dbConfig.pool.idle
+  }
+});
+const db = {};
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+db.auction = require("../components/auction/auction.model.js")(sequelize, Sequelize);
+db.user = require("../components/user/user.model.js")(sequelize, Sequelize, db.auction);
+
+module.exports = db;
